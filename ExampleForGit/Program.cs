b8e2ce2466a2422bbfaea281d63b01fa30add181
OlegStetsula <olegstetsula@gmail.com>
2019-06-19 14:18:17 +0300

    Commit_3

--- ExampleForGit/Program.cs
+++ ExampleForGit/Program.cs
@@ -7,6 +7,7 @@ namespace ExampleForGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello agein!");
         }
     }
 }
