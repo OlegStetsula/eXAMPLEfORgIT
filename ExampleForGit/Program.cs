17451229032250796e6acf5795556c6dc7735a0c
OlegStetsula <olegstetsula@gmail.com>
2019-06-19 15:38:12 +0300

    Commit_5

--- ExampleForGit/Program.cs
+++ ExampleForGit/Program.cs
@@ -8,7 +8,6 @@ namespace ExampleForGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello agein!");
-            Console.WriteLine("Hello dudes!");
             Console.WriteLine("Hello Hello!");
         }
     }
